d10db71b89535f0750c05960d55d300a857aaf3d
Rick Veloz <veloz.ricardo.a@gmail.com>
2018-05-17 21:51:34 -0500

    set up pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    apps: [{
+        name: 'MAIN_MICROS',
+        script: './index.js'
+    }],
+    deploy: {
+        production: {
+            user: 'rick',
+            host: 'ec2-18-188-249-240.us-east-2.compute.amazonaws.com',
+            key: '~/.ssh/aws_dev',
+            ref: 'origin/master',
+            repo: 'git@github.com:rickvlzaa/main_micros.git',
+            path: '/home/rick/microservices/main',
+            'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+        }
+    }
+}
