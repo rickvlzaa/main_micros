1052b20bf678777dbe415271dc4685e3c485f0f5
Rick Veloz <veloz.ricardo.a@gmail.com>
2018-05-17 21:56:10 -0500

    set up server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use('/', (req, res) => {
+    res.send('hello from express')
+})
+
