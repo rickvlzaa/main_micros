d62ae1cfb1044554718cec224a469fcc1256ab03
Rick Veloz <veloz.ricardo.a@gmail.com>
2018-05-18 22:10:06 -0500

    deploy restart

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
     deploy: {
         production: {
             user: 'rick',
-            host: 'ec2-18-188-249-240.us-east-2.compute.amazonaws.com',
+            host: 'ec2-52-14-23-213.us-east-2.compute.amazonaws.com',
             key: '~/.ssh/aws_dev',
             ref: 'origin/master',
             repo: 'git@github.com:rickvlzaa/main_micros.git',
